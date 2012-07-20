057bd09209d9702cafc9a3f991f455d9dabbb4e9
Carl Furrow <me@carlfurrow.com>
2012-07-20 18:18:06 -0400

    Initial commit

---
+++ src/jquery.ajaxTextSaver.js
@@ -0,0 +1,33 @@
+jQuery.fn.ajaxTextSaver = function(url,extra,success,error){
+  var $ = jQuery;
+  var defaults = {
+    id: this.attr("data-id"),
+    event: "blur"
+  };
+  var options = $.extend({},defaults,extra);
+  var dataAttrs = getDataAttributes(this[0]);
+  options = $.extend(options,dataAttrs);
+  
+  this.bind(options.event,function(){
+    var $this = $(this);
+    options.value = $this.val();
+    options = $.extend(options,getDataAttributes(this));
+    $.ajax({
+      url:url,
+      data:options,
+      success:success,
+      error:error
+    });
+  });
+  function getDataAttributes(el){
+    var rDataAttr = /data\-(.+)$/;
+    var attrs = [];
+    $.each(el.attributes,function(index,attr){
+      if(rDataAttr.test(attr.nodeName)){
+        attrs.push(rDataAttr.exec(attr.nodeName)[1]);
+      }
+    });
+    return attrs;
+  }
+};
+$("input").ajaxTextSaver("/",{},function(){alert("success");},function(){alert("fail");});
