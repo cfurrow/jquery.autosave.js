41d89c9f0dab8b9c16c747dc4c81b214fa0acb3c
Carl Furrow <me@carlfurrow.com>
2012-07-22 13:40:55 -0400

    Set options.data with data-* attrs

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -1,22 +1,24 @@
 jQuery.fn.autosave = function(options){
   var $ = jQuery;
   var defaults = {
-    id: this.attr("data-id"),
+    data:{id:this.attr("data-id"),url:""},
     event: "blur",
-    url: "",
     success: function(){},
     error  : function(){}
   };
   options = $.extend(options,defaults,options.extra);
   var dataAttrs = getDataAttributes(this[0]);
-  options = $.extend(options,dataAttrs);
+  options.data = $.extend(options.data,dataAttrs);
+
+  console.log(options);
 
   function getDataAttributes(el){
     var rDataAttr = /data\-(.+)$/;
-    var attrs = [];
+    var attrs = {};
+    attrs.value = el.value;
     $.each(el.attributes,function(index,attr){
       if(rDataAttr.test(attr.nodeName)){
-        attrs.push(rDataAttr.exec(attr.nodeName)[1]);
+        attrs[rDataAttr.exec(attr.nodeName)[1]] = attr.value;
       }
     });
     return attrs;
@@ -24,8 +26,9 @@ jQuery.fn.autosave = function(options){
   
   this.bind(options.event,function(){
     var $this = $(this);
-    options.value = $this.val();
-    options = $.extend(options,getDataAttributes(this));
+    options.data.value = $this.val();
+    options.data = $.extend(options.data,getDataAttributes(this));
+    console.log(options);
     $.ajax({
       url:options.url,
       data:options.data,
