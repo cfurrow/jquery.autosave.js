bb39dec495ebb19f3815cc1905f15f36a0a557ac
Carl Furrow <me@carlfurrow.com>
2012-07-22 13:47:15 -0400

    Add $.each() to iterate over each element found by jquery

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -1,21 +1,39 @@
 jQuery.fn.autosave = function(options){
   var $ = jQuery;
-  var defaults = {
-    data:{id:this.attr("data-id"),url:""},
-    event: "blur",
-    success: function(){},
-    error  : function(){}
-  };
-  options = $.extend(options,defaults,options.extra);
-  var dataAttrs = getDataAttributes(this[0]);
-  options.data = $.extend(options.data,dataAttrs);
+  $.each(this,function(){
+    var $this = $(this);
+    var defaults = {
+      data:{id:$this.attr("data-id"),url:""},
+      event: "blur",
+      success: function(){},
+      error  : function(){}
+    };
+    options = $.extend(options,defaults,options.extra);
+    var dataAttrs = getDataAttributes(this);
+    options.data = $.extend(options.data,dataAttrs);
 
-  console.log(options);
+    console.log(options);
+
+    $this.bind(options.event,function(){
+      var $el = $(this);
+      options.data.value = $el.val();
+      options.data = $.extend(options.data,getDataAttributes(this));
+      console.log(options);
+      $.ajax({
+        url:options.url,
+        data:options.data,
+        success:options.success,
+        error:options.error
+      });
+    });
+  });
 
   function getDataAttributes(el){
     var rDataAttr = /data\-(.+)$/;
     var attrs = {};
     attrs.value = el.value;
+    attrs.name  = el.name;
+
     $.each(el.attributes,function(index,attr){
       if(rDataAttr.test(attr.nodeName)){
         attrs[rDataAttr.exec(attr.nodeName)[1]] = attr.value;
@@ -24,16 +42,4 @@ jQuery.fn.autosave = function(options){
     return attrs;
   }
   
-  this.bind(options.event,function(){
-    var $this = $(this);
-    options.data.value = $this.val();
-    options.data = $.extend(options.data,getDataAttributes(this));
-    console.log(options);
-    $.ajax({
-      url:options.url,
-      data:options.data,
-      success:options.success,
-      error:options.error
-    });
-  });
 };
