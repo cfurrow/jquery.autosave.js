77afd5e424dda4afbe065e9536395859131735f8
Dennis Monsewicz <dennismonsewicz@gmail.com>
2013-07-12 23:07:07 -0500

    adding in new options to support debug mode, support a new data-type attribute for JSON vs HTML ajax dataTypes, and stop the autosave from going anywhere if the original value is the same as the current value when performing the events listed on the input

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -5,27 +5,45 @@ jQuery.fn.autosave = function(options){
     var defaults = {
       data:{},
       event: "change",
+      debug: false,
+      type: "html",
       success: function(){},
       error  : function(){},
       before : function(){}
     };
     options = $.extend(defaults,options);
+    $this.data('original-value', $this.val());
     var data = getDataAttributes(this);
 
     var event = data.event || options.event;
-    $this.on(event,function(){
+    var dataType = data.type || options.type;
+    $this.on(event,function(e){
       var $el = $(this);
       data.value = $el.val();
       data = $.extend(data,getDataAttributes(this));
       var url = data.url ? data.url : options.url;
+      
+      // No need to process the form if nothing actually changed
+      if($el.val() == $el.data('original-value')) {
+        e.preventDefault();
+        return false;
+      }
 
       if(options.before){
         options.before.call(this,$el);
+      }
+      
+      // If Debugging is turned on, log the data var
+      if(options.debug) {
+        console.log(data);
+        e.preventDefault();
+        return false;
       }
 
       $.ajax({
         url:url,
         data:data,
+        dataType: dataType,
         success:function(data){
           options.success(data,$el);
         },
